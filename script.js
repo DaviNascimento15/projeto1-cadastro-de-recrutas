9935dc47ac87f1b4987a448ff7cdec2b0eb62895
Davi Nascimento <davinst19560@gmail.com>
2024-09-11 19:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,4 @@ let curso = prompt('Digite seu curso da base técnica:')
 const anoNascimento = parseInt(prompt('Digite seu ano de nascimento:'))
 let idade = 2024 - anoNascimento
 
-alert(`
-    Olá ${primeiroNome} ${sobrenome}
-    Então você estuda ${curso} e tem ${idade} anos. Legal!
-')
+alert(`Olá ${primeiroNome} ${sobrenome} Então você estuda ${curso} e tem ${idade} anos. Legal`)
